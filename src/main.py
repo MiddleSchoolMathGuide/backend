f55e7fa8dde6ae7b80e70b42bd01d2db65d93a04
Wrench56 <dmarkreg@gmail.com>
2024-07-15 03:01:11 -0400

    Add logging configuration

--- src/main.py
+++ src/main.py
@@ -1,11 +1,28 @@
+'''
+Run backend
+'''
+
+import logging
+import logging.config
+import os
 import uvicorn
 
-from src.utils.const import DEBUG_FLAG, PORT
+from utils.const import DEBUG_FLAG, PORT
 
 
 def main() -> None:
-    uvicorn.run("src.server:app", host="0.0.0.0", port=PORT, reload=DEBUG_FLAG)
+    '''Run backend'''
+    uvicorn.run("server:app", host="0.0.0.0", port=PORT,
+                reload=DEBUG_FLAG, log_config='config/logger.config.ini')
+
+
+def _init_logger() -> None:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    logging.config.fileConfig('config/logger.config.ini')
+    logging.info('Logging init done')
 
 
 if __name__ == '__main__':
+    _init_logger()
     main()
