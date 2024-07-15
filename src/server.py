6d172d14724d2fe64175ab9a9e81a519c46bb862
Wrench56 <dmarkreg@gmail.com>
2024-07-15 03:36:06 -0400

    Add `login` API endpoint with cookie authentication

--- src/server.py
+++ src/server.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import ORJSONResponse
 
+from db.src.auth import login
+
 app = FastAPI()
 
 
 @app.post('/auth/login', response_class=ORJSONResponse)
-async def login(_: Request) -> ORJSONResponse:
-    return ORJSONResponse({'ok': False})
+async def login_api(request: Request) -> ORJSONResponse:
+    data = await request.json()
+    msg, id_ = login.login(data.get('username'), data.get('password_hash'))
+    response = ORJSONResponse(msg)
+    if id_ is not None:
+        response.set_cookie(
+            key='auth_cookie',
+            value=id_,
+        )
+    else:
+        response.status_code = 401
+
+    return response
