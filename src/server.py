d2a0ce7bb22c9d2bb61fd2abb38fccf254868ee1
Wrench56 <dmarkreg@gmail.com>
2024-07-16 18:27:20 -0400

    Add `is_logged_in` API endpoint

--- src/server.py
+++ src/server.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import ORJSONResponse
 
-from db.src.auth import login
+from db.src.auth import login, session
 
 app = FastAPI()
 
@@ -9,14 +9,30 @@ app = FastAPI()
 @app.post('/auth/login', response_class=ORJSONResponse)
 async def login_api(request: Request) -> ORJSONResponse:
     data = await request.json()
-    msg, id_ = login.login(data.get('username'), data.get('password_hash'))
+    username = data.get('username')
+    msg, id_ = login.login(username, data.get('password_hash'))
     response = ORJSONResponse(msg)
     if id_ is not None:
         response.set_cookie(
             key='auth_cookie',
             value=id_,
         )
+        response.set_cookie(
+            key='username',
+            value=username
+        )
     else:
         response.status_code = 401
 
     return response
+
+
+@app.get('/auth/is_logged_in', response_class=ORJSONResponse)
+async def is_logged_in(request: Request) -> ORJSONResponse:
+    if session.is_expired(request.cookies.get('auth_cookie') or ''):
+        return ORJSONResponse(
+            {'ok': False, 'msg': 'Session expired or did not exist'},
+            status_code=401
+        )
+
+    return ORJSONResponse({'ok': True, 'msg': 'User logged in'})
