2620e86939ee123787fcccfd8a963e1bdc98a68e
Wrench56 <dmarkreg@gmail.com>
2024-07-17 01:55:58 -0400

    Add authentication for `user/pages.py`

--- src/routers/user/pages.py
+++ src/routers/user/pages.py
@@ -1,11 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.responses import FileResponse
 
-from const import PAGES
+from const import AUTH_COOKIE, PAGES
+
+from db.src.auth import session
 
 router = APIRouter()
 
 
 @router.get('/user/me', response_class=FileResponse)
-async def login_api() -> FileResponse:
-    return FileResponse(f'{PAGES}/me.html')
+async def login_api(request: Request) -> FileResponse:
+    if session.is_expired(request.cookies.get(AUTH_COOKIE)):
+        return FileResponse(f'{PAGES}/blocked.html')
+    return FileResponse(f'{PAGES}/user/me.html')
