cad6c1598c8df62c1e65b8fddeadf55b85ca67f8
Wrench56 <dmarkreg@gmail.com>
2024-07-18 06:25:24 -0400

    Enhance `edu/editor` page to support dynamic content upload

--- src/routers/edu/editor/pages.py
+++ src/routers/edu/editor/pages.py
@@ -1,17 +1,32 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import FileResponse
+from fastapi.responses import HTMLResponse
+
+import json
 
 from const import AUTH_COOKIE, PAGES
 
 from db.src.auth import session
+from db.src.edu import topics
 
 
 router = APIRouter()
 
 
-@router.get('/edu/editor', response_class=FileResponse)
-async def editor_page(request: Request) -> FileResponse:
+@router.get('/edu/editor{endpoint:path}', response_class=HTMLResponse)
+async def editor_page(request: Request) -> HTMLResponse:
     if session.is_expired(request.cookies.get(AUTH_COOKIE)):
-        return FileResponse(status_code=401)
+        return HTMLResponse(status_code=401)
 
-    return FileResponse(f'{PAGES}/edu/editor.html')
+    topic_name = request.query_params.get('topic')
+
+    with open(f'{PAGES}/edu/editor.html', 'r', encoding='utf-8') as f:
+        content = f.read()
+
+    if topic_name != 'new':
+        topic = topics.get_topic(topic_name)
+        content = content.replace(
+            '<div id="dynamic-data" data=\'\'></div>',
+            f'<div id="dynamic-data" data=\'{json.dumps(topic, default=str)}\'></div>',
+        )
+
+    return HTMLResponse(content)
