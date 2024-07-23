999438303234fcc346e639be94693d3fb014bf8b
Wrench56 <dmarkreg@gmail.com>
2024-07-22 23:57:28 -0400

    Rename `login_api()` to `login_page()`

--- src/routers/auth/pages.py
+++ src/routers/auth/pages.py
@@ -7,5 +7,5 @@ router = APIRouter()
 
 
 @router.get('/login', response_class=FileResponse)
-async def login_api() -> FileResponse:
+async def login_page() -> FileResponse:
     return FileResponse(f'{PAGES}/login.html')
