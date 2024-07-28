2b9efd0c87b06d5b0ac8e0f6fbce1dd3862efb64
Wrench56 <dmarkreg@gmail.com>
2024-07-28 02:25:03 -0400

    Add `/signup` page

--- src/routers/auth/pages.py
+++ src/routers/auth/pages.py
@@ -9,3 +9,8 @@ router = APIRouter()
 @router.get('/login', response_class=FileResponse)
 async def login_page() -> FileResponse:
     return FileResponse(f'{PAGES}/login.html')
+
+
+@router.get('/signup', response_class=FileResponse)
+async def sign_up_page() -> FileResponse:
+    return FileResponse(f'{PAGES}/signup.html')
