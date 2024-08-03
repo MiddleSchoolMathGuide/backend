32aaeb1fd74560c92907c206962571c39f1184ae
Wrench56 <dmarkreg@gmail.com>
2024-08-03 04:22:52 -0400

    Add `/topic/{topic}/{unit}/{lesson}` route

--- src/routers/topic/__init__.py
+++ src/routers/topic/__init__.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 
+from . import api
 from . import pages
 
 from . import editor
 
 topic_routers: tuple[APIRouter, ...] = (
+    api.router,
     pages.router,
     *editor.editor_routers,
 )
---
+++ src/routers/topic/api.py
@@ -0,0 +1,26 @@
+import bson.json_util
+from fastapi import APIRouter, HTTPException, Request
+from fastapi.responses import ORJSONResponse
+
+import json
+import bson
+
+from const import AUTH_COOKIE
+
+from db.src.auth import session
+from db.src.topic import topics
+
+
+router = APIRouter()
+
+
+@router.get('/topic/{topic}/{unit}/{lesson}', response_class=ORJSONResponse)
+async def get_lesson_and_context(
+    request: Request, topic: str, unit: str, lesson: str
+) -> ORJSONResponse:
+    if session.is_expired(request.cookies.get(AUTH_COOKIE) or ''):
+        raise HTTPException(status_code=401, detail='Not authorized')
+
+    return ORJSONResponse(
+        json.loads(bson.json_util.dumps(topics.get_by_titles(topic, unit, lesson)))
+    )
