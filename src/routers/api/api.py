c91613bd9e6a053ca75ead09838eee691c6b79bc
Wrench56 <dmarkreg@gmail.com>
2024-08-03 05:32:03 -0400

    Add `/api/topics` and `/api/unit_context/{topic}/{unit}` API endpoint

--- src/routers/api/api.py
+++ src/routers/api/api.py
@@ -1,26 +1,28 @@
 import bson.json_util
-from fastapi import APIRouter, HTTPException, Request
+from fastapi import APIRouter
 from fastapi.responses import ORJSONResponse
 
 import json
 import bson
 
-from const import AUTH_COOKIE
-
-from db.src.auth import session
 from db.src.topic import topics
 
 
 router = APIRouter()
 
 
-@router.get('/api/{topic}/{unit}/{lesson}', response_class=ORJSONResponse)
-async def get_lesson_and_context(
-    request: Request, topic: str, unit: str, lesson: str
-) -> ORJSONResponse:
-    if session.is_expired(request.cookies.get(AUTH_COOKIE) or ''):
-        raise HTTPException(status_code=401, detail='Not authorized')
+@router.get('/api/t/{topic}/{unit}/{lesson}', response_class=ORJSONResponse)
+async def get_lesson_and_context(topic: str, unit: str, lesson: str) -> ORJSONResponse:
 
     return ORJSONResponse(
         json.loads(bson.json_util.dumps(topics.get_by_titles(topic, unit, lesson)))
     )
+
+
+@router.get('/api/topics', response_class=ORJSONResponse)
+async def get_topics() -> ORJSONResponse:
+    return ORJSONResponse({'ok': True, 'msg': 'Success', 'data': topics.get_all()})
+
+
+@router.get('/api/unit_context/{topic}/{unit}', response_class=ORJSONResponse)
+async def unit_context(unit: str)
