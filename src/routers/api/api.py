78a56c5307a4e9d3e95b065881f1656ad314a942
Wrench56 <dmarkreg@gmail.com>
2024-08-03 06:04:54 -0400

    Add `unit_context()` API endpoint

--- src/routers/api/api.py
+++ src/routers/api/api.py
@@ -5,7 +5,7 @@ from fastapi.responses import ORJSONResponse
 import json
 import bson
 
-from db.src.topic import topics
+from db.src.topic import topics, units
 
 
 router = APIRouter()
@@ -25,4 +25,36 @@ async def get_topics() -> ORJSONResponse:
 
 
 @router.get('/api/unit_context/{topic}/{unit}', response_class=ORJSONResponse)
-async def unit_context(unit: str)
+async def unit_context(topic: str, unit: str) -> ORJSONResponse:
+    topic_res = topics.get_topic_by_title(topic)
+    if topic_res['ok'] is not True:
+        return ORJSONResponse(topic_res)
+    units_ = units.get_all(topic_res['data']['_id'])
+    unit_res = units.get_unit_by_title(unit)
+    if unit_res['ok'] is not True:
+        return ORJSONResponse(unit_res)
+    lessons_ = unit_res['data']['lessons']
+    print(lessons_)
+    print(units_)
+    for i, lesson in enumerate(lessons_):
+        lesson.pop('_id', None)
+        lesson = {
+            k: v
+            for k, v in lesson.items()
+            if k
+            not in (
+                'credits',
+                'icon',
+                'status',
+                'unit_id',
+                'widgets',
+            )
+        }
+
+        lessons_[i] = lesson
+
+    return ORJSONResponse({
+        'ok': True,
+        'msg': 'Success',
+        'data': {'units': units_, 'lessons': lessons_},
+    })
