0f902f0e22d70c2d2520cd12e4ff8856c79c7ba0
Wrench56 <dmarkreg@gmail.com>
2024-08-26 03:45:39 -0400

    Fix static file requests

--- src/const.py
+++ src/const.py
@@ -4,8 +4,9 @@ Constant variables
 
 AUTH_COOKIE = 'auth_cookie'
 
-PAGES = '../public/pages'
-ASSETS = '../public/generated/assets'
+PUBLIC = '../public'
+PAGES = f'{PUBLIC}/pages'
+ASSETS = f'{PUBLIC}/generated/assets'
 
 DEBUG_FLAG: bool = True
 PORT: int = 8000
--- src/server.py
+++ src/server.py
@@ -1,13 +1,35 @@
-from fastapi import FastAPI
+from typing import Awaitable, Callable
+
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
-from const import ASSETS
+import logging
+import os
+
+from const import ASSETS, PUBLIC
 from routers import routers
 from db.src import init
 
 init.init()
 app = FastAPI()
-app.mount('/assets', StaticFiles(directory=f'{ASSETS}'), name='static')
+app.mount("/assets", StaticFiles(directory=f"{ASSETS}"), name="static")
+
+
+@app.middleware("http")
+async def add_process_time_header(
+    request: Request, call_next: Callable[[Request], Awaitable[Response]]
+):
+    response = await call_next(request)
+    if response.status_code == 404 and request.method == "GET":
+        if request.url.components.path.count('/') == 1:
+            filepath = f"{PUBLIC}/generated{request.url.components.path}"
+            if os.path.exists(filepath):
+                logging.info('Redirecting static file GET request')
+                return FileResponse(filepath)
+
+    return response
+
 
 for router in routers:
     app.include_router(router)
